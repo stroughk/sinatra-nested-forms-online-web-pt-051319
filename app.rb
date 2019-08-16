a0c35e351d8c13028b65dabf77e12645974522ad
Kateryna Strough <katerynastrough@hotmail.com>
2019-08-16 00:02:06 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,19 +5,19 @@ module FormsLab
     get '/' do
          erb :root
        end
-   
+
        get '/new' do
          erb :'pirates/new'
        end
-   
+
        post '/pirates' do
          @pirate = Pirate.new(params[:pirate])
-   
+
          params[:pirate][:ships].each do |details|
            Ship.new(details)
          end
          @ships = Ship.all
-   
+
          erb :'pirates/show'
        end
     # code other routes/actions here
